a30dee1418ca2ec89423b3a97e3ccfbd628822f9
Guillian Billard <46932648+nonalphanumeric@users.noreply.github.com>
2022-11-22 11:30:48 +0100

    Update index.spec.ts

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,1 +1,24 @@
+import * as http from 'http'
 
+//test index.ts using jest
+
+
+//test createServer function
+describe('createServer', () => {
+    it('should return a http server', () => {
+        const server = module.require('./index.ts').createServer();
+        expect(server).toBeInstanceOf(http.Server);
+    });
+    }
+);
+
+//test if systerInformation is defined
+describe('systemInformation', () => {
+    it('should be defined', () => {
+        //use systemInformation getter
+        const systemInformation = module.require('./index.ts').getSystemInformation();
+        expect(systemInformation).toBeDefined();
+
+    });
+    }
+);
